fb8985b18ef509df96a29eb087eab4f62d870811
broisatse <sklajn@gmail.com>
2020-05-14 00:33:39 +0100

    Linked stat to main Run class

--- log_parser/run.rb
+++ log_parser/run.rb
@@ -6,13 +6,29 @@ module LogParser
     #    loaders:  list of data loaders. Each data loader should be enumerator yielding address and ip address of the visit
     include Callable
 
-    attr_reader :loaders
-
-    def initialize(loaders:)
+    def initialize(loaders:, stat:)
       @loaders = loaders
+      @stat = stat
     end
 
     def call
-    end    
+      each_log_entry do |path, ip|
+        stat.record path, ip
+      end
+      stat.result
+    end
+
+    private
+
+    # TODO: This should be handled by LoadManager
+    def each_log_entry
+      loaders.each do |loader|
+        loader.each do |path, ip|
+          yield path, ip
+        end
+      end
+    end
+
+    attr_reader :loaders, :stat
   end
 end
--- spec/unit/log_parser/run_spec.rb
+++ spec/unit/log_parser/run_spec.rb
@@ -4,4 +4,37 @@ require 'spec/unit/shared/callable'
 RSpec.describe LogParser::Run do
   it_behaves_like 'callable class'
 
+  let(:loaders) { Array.new(rand 1..3) { generate_fake_loader } }
+  let(:stat) { generate_fake_stat }
+
+  subject(:result) { described_class.new(loaders: loaders, stat: stat).() }
+
+  it 'returns given stat generator result' do
+    expect(result).to eq stat.result
+  end
+
+  it 'records all data from loaders into the stat generator' do
+    result
+
+    loaders.map(&:to_a).inject(:+).group_by(&:itself).each do |(path, ip), visits_ary|
+      expect(stat).to have_received(:record)
+        .with(path, ip)
+        .exactly(visits_ary.count).times
+    end
+  end
+
+  private
+
+  let(:ips) { Array.new(rand 2..4) { Faker::Internet.unique.ip_v4_address } }
+  let(:paths) { Array.new(rand 2..4) { Faker::Lorem.words(number: rand(0..3)).join('/') } }
+
+  def generate_fake_loader
+    Array.new(rand 3..5) { [paths.sample, ips.sample] }.each
+  end
+
+  def generate_fake_stat
+    double 'Fake stat generator',
+      record: nil,
+      result: Array.new(rand 2..3) { [(paths + ips).sample, rand(0..10)] }
+  end
 end
